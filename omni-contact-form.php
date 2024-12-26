001c9a5cffb14b5779ad7578529de14904987897
Demetris Kikizas <d@kikizas.com>
2024-12-26 12:33:34 +0200

    Switch autoloading from spl_autoload_register() to Composer

--- omni-contact-form.php
+++ omni-contact-form.php
@@ -1,80 +1,42 @@
 <?php
 
 /*
-    Omni Contact Form plugin for WordPress
+	Omni Contact Form plugin for WordPress
 
-    Plugin Name:            Omni Contact Form
-    Plugin URI:             https://github.com/demetris/omni-contact-form
-    Description:            A simple contact form with simple shortcode settings
-    Version:                0.4.5
-    Author:                 Demetris Kikizas
-    Author URI:             https://kikizas.com/
-    License:                GPL-2.0
-    License URI:            https://opensource.org/licenses/GPL-2.0
-    Text Domain:            omni-contact-form
-    Domain Path:            /lang
-    Requires PHP:           7.0
-    GitHub Plugin URI:      https://github.com/demetris/omni-contact-form
+	Plugin Name:            Omni Contact Form
+	Plugin URI:             https://github.com/demetris/omni-contact-form
+	Description:            A simple contact form with simple shortcode settings
+	Version:                0.4.5
+	Author:                 Demetris Kikizas
+	Author URI:             https://kikizas.com/
+	License:                GPL-2.0
+	License URI:            https://opensource.org/licenses/GPL-2.0
+	Text Domain:            omni-contact-form
+	Domain Path:            /lang
+	Requires PHP:           7.0
+	GitHub Plugin URI:      https://github.com/demetris/omni-contact-form
 */
 
 namespace OmniContactForm;
 
 /*
 |
-|   Define constants
+| Add autoloader
 |
-|   @since 0.3.0
+| @since 0.5.0
+|
+*/
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+|
+| Define constants
+|
+| @since 0.3.0
 |
 */
 define('OMNI_CONTACT_FORM_DIR', plugin_dir_path(__FILE__));
 define('OMNI_CONTACT_FORM_URI', plugin_dir_url(__FILE__));
-
-/*
-|
-|   Add autoloader
-|   Implementation by Justin Tadlock
-|
-|   @since 0.3.0
-|   @see http://justintadlock.com/archives/2018/12/14/php-namespaces-for-wordpress-developers
-|
-*/
-spl_autoload_register(function($class) {
-    $namespace = 'OmniContactForm\\';
-    $path      = 'src';
-
-    /*
-    |
-    |   Bail if the class is not in our namespace
-    |
-    */
-    if (strpos($class, $namespace) !== 0) {
-        return;
-    }
-
-    /*
-    |
-    |   Remove the namespace
-    |
-    */
-    $class = str_replace($namespace, '', $class);
-
-    /*
-    |
-    |   Build the filename
-    |
-    */
-    $file = realpath(__DIR__ . "/{$path}");
-    $file = $file . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-
-    /*
-    |
-    |   If the file exists for the class name, load it
-    |
-    */
-    if (file_exists($file)) {
-        include($file);
-    }
-});
 
 /**
  *
@@ -85,22 +47,22 @@ spl_autoload_register(function($class) {
  *
  */
 function plugin() {
-    static $instance = null;
+	static $instance = null;
 
-    if (is_null($instance)) {
-        $plugin = new Main();
+	if (is_null($instance)) {
+		$plugin = new Main();
 
-        $plugin->boot();
-    }
+		$plugin->boot();
+	}
 
-    return $instance;
+	return $instance;
 }
 
 /*
 |
-|   Get the plugin up and running
+| Get the plugin up and running
 |
-|   @since 0.3.0
+| @since 0.3.0
 |
 */
 plugin();
